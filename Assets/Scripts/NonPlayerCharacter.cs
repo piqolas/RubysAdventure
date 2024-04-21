2a2461ffc8d843b180deac5855e26ff3792db908
John Connor <jckusho@gmail.com>
2024-04-21 15:29:47 -0400

    Added procedural dialog SFX to NPC interactions
    Co-Authored-By: LucasWP6 <165622110+LucasWP6@users.noreply.github.com>

--- Assets/Scripts/NonPlayerCharacter.cs
+++ Assets/Scripts/NonPlayerCharacter.cs
@@ -1,27 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
 using piqey.Utilities.Editor;
+using piqey.Utilities;
 
 namespace piqey
 {
 	public class NonPlayerCharacter : MonoBehaviour
 	{
 		public GameObject DialogBox;
-		[Min(0.0f)]
-		public float DisplayTime = 5.0f;
+		public AudioSource DialogAudioSource;
+		[Min(0.0f)] public float DialogBoxDisplayTime = 5.0f;
+		[Min(0.0f)] public float DialogTalkTime = 2.0f;
+		public AudioClip[] DialogSounds;
 
 		[SerializeField, ReadOnly, Label("_lastDisplayed")]
 		private float _lastDisplayed = 0.0f;
+		private Bucket<AudioClip> _dialogBucket;
+
+		void Start()
+		{
+			if (DialogAudioSource == null)
+				DialogAudioSource = gameObject.AddComponent<AudioSource>();
+
+			_dialogBucket = new(new List<AudioClip>(DialogSounds));
+		}
 
 		void Update()
 		{
-			if (DialogBox.activeSelf && Time.time - _lastDisplayed >= DisplayTime)
-				DialogBox.SetActive(false);
+			if (DialogBox.activeSelf)
+			{
+				if (Time.time - _lastDisplayed >= DialogBoxDisplayTime)
+					DialogBox.SetActive(false);
+				else if (!DialogAudioSource.isPlaying && Time.time - _lastDisplayed <= DialogTalkTime)
+					DialogAudioSource.PlayOneShot(_dialogBucket.Sample());
+			}
 		}
 
 		public void DisplayDialog()
 		{
 			_lastDisplayed = Time.time;
 			DialogBox.SetActive(true);
+			DialogAudioSource.Stop();
 		}
 	}
 }
